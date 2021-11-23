5cc7fced9dd270a6bbb84f6d0dac9a54851a4563
SantiYagito22 <57524451+SantiYagito22@users.noreply.github.com>
2021-11-23 14:25:00 +0100

    Implementacion resta

--- hellowordl.java
+++ hellowordl.java
@@ -1,5 +1,10 @@
 public class HolaMundo {
   public static void main (String[] args) {
     System.out.println("Hola, mundo!");
+    
+    int a=3;
+    int b=5;
+    int c=a-b;
+    System.out.println("La resta es de "+c.toString());
   }
 }
