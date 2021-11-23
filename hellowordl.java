8b63cd2dc0e1247d9df0b53f66c94be8026e824a
SantiYagito22 <santiyagito22@gmail.com>
2021-11-23 14:27:54 +0100

    Implementacion suma

--- hellowordl.java
+++ hellowordl.java
@@ -1,5 +1,9 @@
 public class HolaMundo {
   public static void main (String[] args) {
     System.out.println("Hola, mundo!");
+    int a=3;
+    int b=5;
+    int c=a+b;
+    System.out.println("La suma es de "+c.toString());
   }
 }
